f0b3bb09f8a54a4d4bef330b9ce78fd1aa824687
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-03-22 17:32:08 -0700

    feat: add cohost integration

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,12 +5,37 @@ import { Header } from "@/components/Header.tsx";
 import Pond from "@/islands/Pond.tsx";
 import Psst from "@/islands/secrets/Psst.tsx";
 
+async function getThoughts() {
+  try {
+    const req = await fetch(
+      "https://cohost.org/api/v1/project/linolevan/posts",
+      {
+        headers: {
+          cookie: `connect.sid=${Deno.env.get("COHOST_KEY")}`,
+        },
+      },
+    );
+    const res = await req.json();
+
+    return res.items as {
+      publishedAt: string;
+      plainTextBody: string;
+      singlePostPageUrl: string;
+      postingProject: { handle: string; avatarURL: string };
+    }[];
+  } catch {
+    return [];
+  }
+}
+
 export default async function Home() {
   const req = await fetch(
     "https://raw.githubusercontent.com/lino-levan/lino-levan/main/README.md",
   );
   const res = await req.text();
   const text = res.split("Hi there\n")[1].split("###")[0];
+
+  const thoughts = await getThoughts();
 
   return (
     <>
@@ -28,6 +53,29 @@ export default async function Home() {
             class="prose lg:prose-xl px-4"
             dangerouslySetInnerHTML={{ __html: render(text) }}
           />
+          <div className="py-8 prose lg:prose-xl">
+            <h2>Recent Thoughts</h2>
+          </div>
+          <div class="flex flex-col gap-2 max-w-screen-md w-full pb-4">
+            {thoughts.map((thought) => (
+              <div class="flex items-center gap-2 w-full bg-teal-200 p-4 rounded-lg">
+                <img src={thought.postingProject.avatarURL} class="w-10 h-10" />
+                <div class="flex flex-col">
+                  <div class="flex gap-2 items-center">
+                    <a href="https://cohost.org/linolevan" class="font-bold">
+                      @{thought.postingProject.handle}
+                    </a>{" "}
+                    <span>
+                      {new Intl.DateTimeFormat("en-US").format(
+                        new Date(thought.publishedAt),
+                      )}
+                    </span>
+                  </div>
+                  <p>{thought.plainTextBody}</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </>
